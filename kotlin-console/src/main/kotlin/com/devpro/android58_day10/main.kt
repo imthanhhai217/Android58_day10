58ae29a353e1494a8006c46eab0877bff4a06817
Juhalion <imthanhhai217@gmail.com>
2026-05-29 21:11:39 +0700

    Add homework exercises and comments for future implementation

--- kotlin-console/src/main/kotlin/com/devpro/android58_day10/main.kt
+++ kotlin-console/src/main/kotlin/com/devpro/android58_day10/main.kt
@@ -215,3 +215,11 @@ fun isPrime(number: Int): Boolean {
     }
     return true
 }
+
+
+// Bài tập về nhà
+// Bài 1: Compress chuỗi eg: "aaabbcaaa" -> "a3b2ca3", "abc" -> "abc", "aabbcc" -> "a2b2c2"
+// Bài 2: Tính giai thừa của một số nguyên dương n (n!) = 1*2*3*...*n
+// Bài 3: Tìm số lớn thứ nhì trong list, không sử dụng hàm có sẵn
+// Bài 4: Tìm độ dài chuỗi liên tiếp tăng  dài nhất. eg: [1, 3, 5, 4, 7, 8, 9, 2] → 4 (chuỗi 4,7,8,9)
+// Bài 5: Chuyển số La Mã thành số nguyên. eg: "XII" -> 12, "IX" -> 9, "LVIII" -> 58
